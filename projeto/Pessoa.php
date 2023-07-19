a8c8df850bce2089a30df7a9a6ce61be33baed61
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-07-18 22:14:34 -0300

    classe pessoa criada

---
+++ projeto/Pessoa.php
@@ -0,0 +1,89 @@
+<?php
+
+class Pessoa
+{
+    private $id;
+    private $nome;
+    private $email;
+    private $telefone;
+
+    /**
+     * @param $nome
+     * @param $email
+     * @param $telefone
+     */
+    public function __construct($nome, $email, $telefone)
+    {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->telefone = $telefone;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param string $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * @param string $telefone
+     */
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+    }
+
+
+
+
+}
